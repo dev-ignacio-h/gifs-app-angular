53adcb67e000a8a15d2a1a70bf2ef847d194d3d9
Ignacio <dev.ignacio.h@gmail.com>
2021-01-28 20:36:20 -0300

    fix typo

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('gifts-app app is running!');
+    expect(await page.getTitleText()).toEqual('gifs-app app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/gifts-app'),
+      dir: require('path').join(__dirname, './coverage/gifs-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
