37f71814ec6df58826377933c1d12abc2fd70179
Ignacio <dev.ignacio.h@gmail.com>
2021-01-31 23:13:14 -0300

    http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GifsService {
+  private apiKey: string = 'api_key=Br7Gb6AcrEfll1eULoqVIYzzceq2m6JT';
+  private baseUrl: string = 'https://api.giphy.com/v1/gifs';
   private _record: string[] = [];
 
   get record() {
     return [...this._record];
   }
+
+  constructor(private http: HttpClient) {}
 
   searchGifs(query: string) {
     query = query.trim().toLowerCase();
@@ -16,6 +21,11 @@ export class GifsService {
       this._record.unshift(query);
       this._record = this._record.splice(0, 10);
     }
-    console.log(this._record);
+
+    this.http
+      .get(`${this.baseUrl}/search?${this.apiKey}&q=${query}&limit=12`)
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+      });
   }
 }
